df84a3f46acef924b8ddc6d2b9727b7deeab6ed1
spiglebach <spiglebach@gmail.com>
2022-10-22 19:54:34 +0200

    move loyalty endpoint

---
+++ backend/src/main/java/com/example/backend/LoyaltyController.java
@@ -0,0 +1,67 @@
+package com.example.backend;
+
+import com.example.backend.dto.CustomerLoyalty;
+import com.example.backend.dto.CustomerRewards;
+import com.example.backend.schema.Event;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.AggregationOptions;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.project;
+
+@RestController
+@RequestMapping("/loyalty")
+@RequiredArgsConstructor
+public class LoyaltyController {
+    private final MongoTemplate mongoTemplate;
+
+    @GetMapping("/")
+    public ResponseEntity<?> getLoyaltyInDays() {
+        var options = AggregationOptions.builder().allowDiskUse(true).build();
+        var aggregation = newAggregation(
+                //match(Criteria.where("customer").is(391705)),
+                project("id", "date", "action")
+                        .andExpression("toString(customer)").as("customer")
+                        .and("properties.subtotal").as("money"),
+                group("customer")
+                        .min("date").as("optInDate")
+                        .max("date").as("lastEventDate")
+                        .sum("money").as("spending")
+                ,
+                limit(1000),
+                project("optInDate", "lastEventDate", "spending")
+                        .and("lastEventDate").minus("optInDate").as("loyalDays")
+                        .and("customer").previousOperation()
+        ).withOptions(options);
+        var result = mongoTemplate.aggregate(aggregation, Event.class, CustomerLoyalty.class).getMappedResults();
+        //var result = mongoTemplate.aggregate(aggregation, Event.class, Map.class);
+        var customerIds = result.stream().map(CustomerLoyalty::getCustomer).toList();
+        var customerRewards = mongoTemplate.aggregate(
+                newAggregation(
+                        project("id", "action").andExpression("toString(customer)").as("customer"),
+                        match(Criteria.where("customer").in(customerIds).and("action").is("reward")),
+                        group("customer").count().as("rewards"),
+                        project("rewards").and("customer").previousOperation()
+                ).withOptions(options),
+                Event.class,
+                CustomerRewards.class
+        ).getMappedResults();
+        return ResponseEntity.ok(result
+                .stream().map(cl -> {
+                    cl.setLoyalDays(cl.getLoyalDays() / 86_400_000);
+                    cl.setRewardsClaimed(customerRewards.stream()
+                            .filter(cr -> Objects.equals(cr.getCustomer(), cl.getCustomer()))
+                            .findFirst().map(CustomerRewards::getRewards).stream().findFirst().orElse(0L));
+                    return cl;
+                }).collect(Collectors.toList()));
+    }
+}
--- backend/src/main/java/com/example/backend/PeriodAnalysisController.java
+++ backend/src/main/java/com/example/backend/PeriodAnalysisController.java
@@ -1,14 +1,11 @@
 package com.example.backend;
 
-import com.example.backend.dto.CustomerLoyalty;
-import com.example.backend.dto.CustomerRewards;
 import com.example.backend.dto.DailyExpire;
 import com.example.backend.dto.PointsByDate;
 import com.example.backend.schema.Event;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.aggregation.AggregationOptions;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
@@ -17,8 +14,6 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
 import static org.springframework.data.mongodb.core.aggregation.Aggregation.sort;
@@ -75,46 +70,5 @@ public class PeriodAnalysisController {
         );
         var dailyExpires = mongoTemplate.aggregate(aggregation, Event.class, DailyExpire.class);
         return ResponseEntity.ok(dailyExpires.getMappedResults());
-    }
-
-    @GetMapping("/loyalty")
-    public ResponseEntity<?> getLoyaltyInDays() {
-        var options = AggregationOptions.builder().allowDiskUse(true).build();
-        var aggregation = newAggregation(
-                //match(Criteria.where("customer").is(391705)),
-                project("id", "date", "action")
-                        .andExpression("toString(customer)").as("customer")
-                        .and("properties.subtotal").as("money"),
-                group("customer")
-                        .min("date").as("optInDate")
-                        .max("date").as("lastEventDate")
-                        .sum("money").as("spending")
-                ,
-                limit(1000),
-                project("optInDate", "lastEventDate", "spending")
-                        .and("lastEventDate").minus("optInDate").as("loyalDays")
-                        .and("customer").previousOperation()
-        ).withOptions(options);
-        var result = mongoTemplate.aggregate(aggregation, Event.class, CustomerLoyalty.class).getMappedResults();
-        //var result = mongoTemplate.aggregate(aggregation, Event.class, Map.class);
-        var customerIds = result.stream().map(CustomerLoyalty::getCustomer).toList();
-        var customerRewards = mongoTemplate.aggregate(
-                newAggregation(
-                        project("id", "action").andExpression("toString(customer)").as("customer"),
-                        match(Criteria.where("customer").in(customerIds).and("action").is("reward")),
-                        group("customer").count().as("rewards"),
-                        project("rewards").and("customer").previousOperation()
-                ).withOptions(options),
-                Event.class,
-                CustomerRewards.class
-        ).getMappedResults();
-        return ResponseEntity.ok(result
-                .stream().map(cl -> {
-                    cl.setLoyalDays(cl.getLoyalDays() / 86_400_000);
-                    cl.setRewardsClaimed(customerRewards.stream()
-                            .filter(cr -> Objects.equals(cr.getCustomer(), cl.getCustomer()))
-                            .findFirst().map(CustomerRewards::getRewards).stream().findFirst().orElse(0L));
-                    return cl;
-                }).collect(Collectors.toList()));
     }
 }
