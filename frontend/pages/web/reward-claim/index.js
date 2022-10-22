159f053bc8df4767f470a74e176af647e3e8b674
Gergely Szukács <szukacs.gergely@gmail.com>
2022-10-22 13:47:30 +0200

    activity percentage chart added

---
+++ frontend/pages/web/reward-claim/index.js
@@ -0,0 +1,14 @@
+import {RewardClaim} from "../../../components/RewardClaim";
+
+const RewardClaimPage = (props) => {
+  return(
+      <>
+      <h1>
+          Reward Claimed
+      </h1>
+          <RewardClaim />
+      </>
+  );
+}
+
+export default RewardClaimPage;
